fb4520f7977b2c4473d144605d096555503a37f4
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2019-02-25 07:49:22 -0600

    Upload 2

--- r-directory/mwhit.R
+++ r-directory/mwhit.R
@@ -1,2 +1,18 @@
 library(dplyr)
 library(readr)
+library(purrr)
+library(nycflights13)
+
+# understand <- read_csv("understand.csv")
+# G = read.csv("understand.csv")
+# by_day <- group_by(flights, year, month, day)
+# summarise(by_day, delay = mean(dep_delay, na.rm = TRUE))
+# by_dest <- group_by(flights, dest)
+
+write.csv(mtcars, "mtcars.csv")
+write.csv(mtcars, "mtcars.csv", row.names=FALSE)
+mainT <- read_csv("mtcars.csv")
+mainTT <- tibble(mtcars)
+
+#do 6 cylinder cars have equal weight as 4 cylinder cars? 
+# to 
